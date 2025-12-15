299de93abe61b6e392e6ab8dcf184e19ee5e6008
devsujalpatel <sujal7455@gmail.com>
2025-12-15 23:56:40 +0530

    Better auth setup

---
+++ src/db/index.ts
@@ -0,0 +1,19 @@
+import {
+  account,
+  accountRelations,
+  session,
+  sessionRelations,
+  user,
+  userRelations,
+  verification,
+} from "./auth-schema";
+
+export {
+  account,
+  accountRelations,
+  session,
+  sessionRelations,
+  user,
+  userRelations,
+  verification,
+};
