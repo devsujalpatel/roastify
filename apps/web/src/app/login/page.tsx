c6ae68c27d43c74d88bc40a00dd6f1f23c51fe1f
devsujalpatel <sujal7455@gmail.com>
2025-12-23 07:19:53 +0530

    back to next js

--- apps/web/src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignInForm from "@/components/sign-in-form";
-
-export default function LoginPage() {
-  return (
-    <div className="flex items-center justify-center h-[90vh]">
-      <SignInForm />
-    </div>
-  );
-}
---
+++ src/lib/auth.ts
@@ -0,0 +1,26 @@
+import { betterAuth } from "better-auth";
+import { db } from "@/config";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "pg" or "mysql"
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+    spotify: {
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+    },
+  },
+
+  /** if no database is provided, the user data will be stored in memory.
+   * Make sure to provide a database to persist user data **/
+});
