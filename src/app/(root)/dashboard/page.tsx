18b76b2ad84854e490c1f294d071033a37e5198e
devsujalpatel <sujal7455@gmail.com>
2025-12-27 17:01:46 +0530

    fetching data in client comp using useTrpc and useQuery

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,15 +1,20 @@
-import { caller } from "@/trpc/server";
+"use client";
 
-const Dashboard = async () => {
-  const user = await caller.getUser();
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+const Dashboard = () => {
+  const trpc = useTRPC();
+  const { data: user } = useQuery(trpc.getUser.queryOptions());
 
   return (
     <div>
-      {user.map((u) => (
-        <div>
-          <h1 key={u.id}>Welcome, {u.name}!</h1>
-        </div>
-      ))}
+      {user &&
+        user.map((u) => (
+          <div key={u.id}>
+            <h1 key={u.id}>Welcome, {u.name}!</h1>
+          </div>
+        ))}
     </div>
   );
 };
